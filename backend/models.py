08dd5ceb2907299812e52a3411a82f1188fbbc8c
Amy <amypirro@gmail.com>
2025-01-09 21:50:41 -0800

    added endpoint to get bat events by player bam_id

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
 from typing import Literal
 
@@ -8,8 +8,36 @@ class Batter(BaseModel):
     batter_name_first: str
     batter_name_last: str
 
+
 class Batters(BaseModel):
     batters: list[Batter]
+
+
+class BatterEvent(BaseModel):
+    batter_bam_id: int
+    batter_name_first: str
+    batter_name_last: str
+    event_type: str | None
+    bottom: bool
+    event_counts: int | None
+
+class BatEvents(BaseModel):
+    strikeout: int
+    single: int
+    double: int
+    triple: int
+    home_run: int
+    walk: int
+    field_out: int
+
+class BatterEvents(BaseModel):
+    batter_events: list[BatterEvent]
+
+class BatterEventsTest(BaseModel):
+    batter: Batter
+    home_events: BatEvents
+    away_events: BatEvents
+
 
 
 
--- backend/pitch_routers.py
+++ backend/pitch_routers.py
@@ -10,7 +10,8 @@ import sqlalchemy as sqla
 from sqlalchemy.orm import Session
 from database import get_db
 from db_models import Pitch
-from models import Batters
+from models import Batter, Batters, BatterEvent, BatterEvents, BatterEventsTest
+import json
 
 
 router = APIRouter(tags=["Pitches"], prefix="/api")
@@ -54,6 +55,69 @@ def get_padres_batters(db: Session = Depends(get_db)):
         )
 
     return {"batters": batters}
+
+
+@router.get("/batters/{batterBam}/events", response_model=BatterEventsTest)
+def get_batter_events(batterBam: int, db: Session = Depends(get_db)):
+    try:
+        bat_events = (
+            db.query(
+                Pitch.batter_bam_id,
+                Pitch.batter_name_first,
+                Pitch.batter_name_last,
+                Pitch.event_type,
+                Pitch.bottom,
+                sqla.func.count(Pitch.event_type).label("event_counts"),
+            )
+            .filter(
+                sqla.and_(Pitch.batter_bam_id == batterBam, Pitch.terminating == True)
+            )
+            .group_by(Pitch.event_type, Pitch.bottom)
+            .all()
+        )
+    except:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Could not retrieve batter events",
+        )
+
+    if not bat_events:
+        raise HTTPException(
+            status_code=404, detail="Player not found or no bat events for player"
+        )
+
+    event_categories = {
+        "strikeout",
+        "single",
+        "double",
+        "triple",
+        "home_run",
+        "walk",
+        "field_out",
+    }
+    home_events = {x: 0 for x in event_categories}
+    away_events = {x: 0 for x in event_categories}
+
+    for p in bat_events:
+        if p.event_type in event_categories:
+            if p.bottom == True:
+                home_events[p.event_type] = p.event_counts
+            elif p.bottom == False:
+                away_events[p.event_type] = p.event_counts
+
+    # print("***************** BATTERS!!!!!", bat_events)
+    # print("@@@@@@@@@@@@@@@@@ home_events:", home_events)
+    # print("&&&&&&&&&&&&&&&&& away_events:", away_events)
+    # return {"batter_events": bat_events}
+    return {
+        "batter": Batter(
+            batter_bam_id=bat_events[0].batter_bam_id,
+            batter_name_first=bat_events[0].batter_name_first,
+            batter_name_last=bat_events[0].batter_name_last,
+        ),
+        "home_events": home_events,
+        "away_events": away_events,
+    }
 
 
 # @router.get("pitches/to-padres")
